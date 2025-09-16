3f608be1afa200dcd8c0e71f7e6f4f41cb62bdd8
System Administrator <root@apples-MacBook-Pro.local>
2025-09-16 18:09:09 +0530

    my 11th commit

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
         />
         <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
           <li className="mb-2 tracking-[-.01em]">
-            lallan borasi@ deep b this is my project appReal-Time Feedback Our health and wellness apps deliver real-time feedback to help users improve habits instantly. Whether you're using a fasting tracker or a calorie counter, get actionable insights as you go—so you can stay consistent.  this mi my update fro hispl AVV @^^ @@@@@@this is my project 
+            lallan borasi@ deep b this is my project appReal-Time Feedback Our health and wellness apps deliver real-time feedback to help users improve habits instantly. Whether you're using a fasting tracker or a calorie counter, get actionable insights as you go—so you can stay consistent.  this mi my update fro hispl AVV @^^ @@@@@@this is my project from lallanborasi
              {"MY REACT AAP"}
             <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
               app/me.tsx
